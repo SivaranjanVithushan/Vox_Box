73f564e94f20eb206fc44cb0e3e642608502af28
Sivaranjan Vithushan <ict19868@fot.sjp.ac.lk>
2024-06-29 14:14:48 +0530

    refactor: Update Firebase Realtime Database initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/constants/routes.dart';
 import 'core/theme_data/colour_scheme.dart';
 import 'core/theme_data/text_theme.dart';
 
+// Firebase Realtime Database URL
+const String firebaseDatabaseURL =
+    'https://voxbox-8da27-default-rtdb.firebaseio.com/';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseDatabase.instanceFor(
+    app: Firebase.app(),
+    databaseURL: firebaseDatabaseURL,
+  );
   runApp(ProviderScope(child: const MainApp()));
 }
 
@@ -22,9 +31,7 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: lightColorScheme,
-        //Default font
-        fontFamily: 'Poppins',
-        //Font Themes
+        // Font Themes
         textTheme: textTheme,
         // Elevated button default theme
       ),
