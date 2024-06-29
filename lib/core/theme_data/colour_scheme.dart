da4e36eb7ed7ad084342be9d6d2347c4764639be
Sivaranjan Vithushan <ict19868@fot.sjp.ac.lk>
2024-06-29 14:20:41 +0530

    refactor: Update color scheme and text theme

--- lib/core/theme_data/colour_scheme.dart
+++ lib/core/theme_data/colour_scheme.dart
@@ -1,12 +1,12 @@
+// Theme Colors Used
 import 'package:flutter/material.dart';
 
-//Theme Colours Used
 Color primarybackground = const Color(0xff34A853);
 Color secondarybackground = Colors.white;
 Color primaryText = const Color(0xff34A853);
 Color secondaryText = const Color(0xff000000);
 
-//Custom Colurs
+// Custom Colors
 Color darkGreen = const Color.fromARGB(255, 7, 148, 85);
 Color lightGreen = const Color.fromARGB(255, 219, 250, 230);
 Color inputBorderColor = const Color.fromARGB(255, 208, 213, 221);
@@ -15,20 +15,22 @@ Color inputTextColor = const Color.fromARGB(255, 16, 24, 40);
 Color suffixIconColor = const Color.fromARGB(255, 152, 162, 179);
 Color errorColor2 = const Color.fromARGB(255, 217, 45, 32);
 
-//Light theme data
+// Light theme data
 final ColorScheme lightColorScheme = ColorScheme(
-    brightness: Brightness.light,
-    primary: primarybackground,
-    onPrimary: Colors.white,
-    secondary: primarybackground,
-    onSecondary: primaryText,
-    error: Colors.red,
-    onError: Colors.white,
-    surface: secondarybackground,
-    onSurface: primaryText,
-    onTertiary: Colors.white);
+  brightness: Brightness.light,
+  primary: primarybackground,
+  onPrimary: Colors.white,
+  secondary: primarybackground,
+  onSecondary: primaryText,
+  error: Colors.red,
+  onError: Colors.white,
+  surface: secondarybackground,
+  onSurface: primaryText,
+  background: secondarybackground,
+  onBackground: primaryText,
+);
 
-//Dark theme data -- NOT COMPLETE !
+// Dark theme data
 final ColorScheme darkColorScheme = ColorScheme(
   brightness: Brightness.dark,
   primary: Colors.blue,
@@ -39,4 +41,6 @@ final ColorScheme darkColorScheme = ColorScheme(
   onError: Colors.white,
   surface: Colors.purple.shade100,
   onSurface: Colors.white,
+  background: Colors.black,
+  onBackground: Colors.white,
 );
