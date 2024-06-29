47e7905652664de64fa95e6528b8552efdf602cc
Sivaranjan Vithushan <ict19868@fot.sjp.ac.lk>
2024-06-29 15:26:35 +0530

    refactor: Update CustomAppBar to center title and change foreground color

--- lib/modules/shared_widget/top_app_bar.dart
+++ lib/modules/shared_widget/top_app_bar.dart
@@ -17,27 +17,26 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      centerTitle: true,
+      foregroundColor: secondarybackground,
       backgroundColor: Colors.green,
-      leading: IconButton(
-        icon: Icon(Icons.arrow_back, color: secondarybackground),
-        onPressed: onLeftArrowPressed,
-      ),
-      title: Center(
-        child: Text(
-          title,
-          style: Theme.of(context)
-              .textTheme
-              .titleLarge!
-              .copyWith(color: secondarybackground),
-        ),
+      title: Text(
+        title,
+        style: Theme.of(context)
+            .textTheme
+            .titleLarge!
+            .copyWith(color: secondarybackground),
       ),
       actions: [
-        IconButton(
-          icon: Icon(
-            Icons.arrow_forward,
-            color: Colors.transparent,
+        Visibility(
+          visible: onRightArrowPressed != null,
+          child: IconButton(
+            icon: Icon(
+              Icons.arrow_forward,
+              color: secondarybackground,
+            ),
+            onPressed: onRightArrowPressed,
           ),
-          onPressed: onRightArrowPressed,
         ),
       ],
     );
