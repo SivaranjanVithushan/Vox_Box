dc15fb148685692c1571e70aace90ca5d61b0eef
Sivaranjan Vithushan <ict19868@fot.sjp.ac.lk>
2024-06-29 14:14:09 +0530

    refactor: Update CreateRoomDialog to use Form and TextFormField for better input validation

--- lib/modules/home/presenters/widgets/create_room_dialog.dart
+++ lib/modules/home/presenters/widgets/create_room_dialog.dart
@@ -9,43 +9,93 @@ class CreateRoomDialog extends ConsumerStatefulWidget {
 }
 
 class _CreateRoomDialogState extends ConsumerState<CreateRoomDialog> {
-  final TextEditingController _controller = TextEditingController();
-  String? _errorMessage;
+  final _formKey = GlobalKey<FormState>();
+  String _boxName = '';
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = ref.watch(chatRoomViewModelProvider);
+
     return AlertDialog(
-      title: Text('Create New Room'),
+      insetPadding: EdgeInsets.symmetric(horizontal: 16),
+      titlePadding: EdgeInsets.only(bottom: 20, top: 30, left: 16, right: 16),
+      title: Center(
+          child: Text(
+        'Create Room',
+        style: textTheme.titleLarge!.copyWith(
+          color: Colors.black,
+        ),
+      )),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          TextField(
-            style: textTheme.labelMedium,
-            controller: _controller,
-            decoration: InputDecoration(
-              hintText: 'Room Name',
-              errorText: _errorMessage,
+          Form(
+            key: _formKey,
+            child: TextFormField(
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(color: Colors.black),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.only(top: 16, bottom: 16, left: 16),
+                hintText: 'Box Name',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide:
+                        BorderSide(color: Color(0xff34A853), width: 1.0)),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a room name';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _boxName = value!;
+              },
             ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            width: double.infinity,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.only(top: 14, bottom: 14),
+                  foregroundColor: Colors.white,
+                  backgroundColor: Color(0xff34A853),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    viewModel.createRoom(_boxName).then((result) {
+                      if (result == null) {
+                        Navigator.of(context).pop();
+                        Navigator.pushNamed(
+                          context,
+                          '/chat',
+                          arguments: {
+                            'roomName': _boxName,
+                            'roomId': viewModel.latestRoomId
+                          },
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text(result)),
+                        );
+                      }
+                    });
+                  }
+                },
+                child: Text(
+                  'Create',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                )),
           ),
         ],
       ),
-      actions: [
-        ElevatedButton(
-          onPressed: () async {
-            final errorMessage = await ref
-                .read(chatRoomViewModelProvider)
-                .createRoom(_controller.text.trim());
-            if (errorMessage != null) {
-              setState(() {
-                _errorMessage = errorMessage;
-              });
-            } else {
-              Navigator.of(context).pop();
-            }
-          },
-          child: Text('Create'),
-        ),
-      ],
     );
   }
 }
--- lib/modules/home/presenters/widgets/join_room_dialog.dart
+++ lib/modules/home/presenters/widgets/join_room_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vox_box/core/theme_data/text_theme.dart';
 import 'package:vox_box/modules/home/presenters/chatroom_contoller.dart';
 
 class JoinRoomDialog extends ConsumerStatefulWidget {
@@ -8,46 +9,96 @@ class JoinRoomDialog extends ConsumerStatefulWidget {
 }
 
 class _JoinRoomDialogState extends ConsumerState<JoinRoomDialog> {
-  final TextEditingController _controller = TextEditingController();
-  String? _errorMessage;
+  final _formKey = GlobalKey<FormState>();
+  String _boxId = '';
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = ref.watch(chatRoomViewModelProvider);
+
     return AlertDialog(
-      title: Text('Join Room'),
+      titlePadding: EdgeInsets.only(bottom: 20, top: 30, left: 16, right: 16),
+      title: Center(
+        child: Text('Join Room',
+            style: textTheme.titleLarge!.copyWith(
+              color: Colors.black,
+            )),
+      ),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          TextField(
-            controller: _controller,
-            decoration: InputDecoration(
-              hintText: 'Room ID',
-              errorText: _errorMessage,
+          Form(
+            key: _formKey,
+            child: TextFormField(
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(color: Colors.black),
+              // controller: _controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide:
+                        BorderSide(color: Color(0xff34A853), width: 1.0)),
+                contentPadding: EdgeInsets.only(top: 16, bottom: 16, left: 16),
+                hintText: 'Box ID',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a box ID';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _boxId = value!;
+              },
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.only(top: 14, bottom: 14),
+                foregroundColor: Colors.white,
+                backgroundColor: Color(0xff34A853),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                  viewModel.joinRoom(_boxId).then((result) {
+                    if (result != null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(result),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                    } else {
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushNamed(
+                        '/chat',
+                        arguments: {
+                          'roomId': _boxId,
+                          'roomName':
+                              'Room Name' // Fetch room name if necessary
+                        },
+                      );
+                    }
+                  });
+                }
+              },
+              child: Text(
+                'Join',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
             ),
           ),
         ],
       ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: Text('Cancel'),
-        ),
-        ElevatedButton(
-          onPressed: () async {
-            final errorMessage = await ref
-                .read(chatRoomViewModelProvider)
-                .joinRoom(_controller.text.trim());
-            if (errorMessage != null) {
-              setState(() {
-                _errorMessage = errorMessage;
-              });
-            } else {
-              Navigator.of(context).pop();
-            }
-          },
-          child: Text('Join'),
-        ),
-      ],
     );
   }
 }
