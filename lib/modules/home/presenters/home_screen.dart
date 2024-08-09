8ce1ec9cfeb21a68267b4fe3ebd239de82f15aa8
Sivaranjan Vithushan <ict19868@fot.sjp.ac.lk>
2024-08-09 09:44:49 +0530

    refactor: Update Firebase configuration and add chat room functionality

--- lib/modules/home/presenters/home_screen.dart
+++ lib/modules/home/presenters/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:vox_box/modules/chatUi/presenters/chat_screen.dart';
 import 'package:vox_box/modules/home/presenters/widgets/create_room_dialog.dart';
 import 'package:vox_box/modules/home/presenters/widgets/join_room_dialog.dart';
 import 'package:vox_box/modules/shared_widget/top_app_bar.dart';
@@ -11,10 +13,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // TextEditingController _createBoxController = TextEditingController();
-  // TextEditingController _joinBoxController = TextEditingController();
-  // String? _createErrorMessage;
-  // String? _joinErrorMessage;
+  final DatabaseReference _chatRoomsRef =
+      FirebaseDatabase.instance.ref().child('chat_rooms');
+  List<Map<String, dynamic>> _chatRooms = [];
+  bool _isLoading = true;
+  String? _errorMessage;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchChatRooms();
+  }
+
+  void _fetchChatRooms() {
+    _chatRoomsRef.onValue.listen((event) {
+      final data = Map<String, dynamic>.from(event.snapshot.value as Map);
+      final List<Map<String, dynamic>> rooms = [];
+      data.forEach((key, value) {
+        rooms.add({'id': key, 'name': value['name']});
+      });
+      setState(() {
+        _chatRooms = rooms;
+        _isLoading = false;
+        _errorMessage = null;
+      });
+    }).onError((error) {
+      setState(() {
+        _isLoading = false;
+        _errorMessage = 'Failed to load chat rooms. Please try again later.';
+      });
+    });
+  }
 
   void _showPopupMenu(BuildContext context, Offset offset) async {
     await showMenu(
@@ -78,10 +107,13 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text("Please create a new box or join an existing one",
                 style: Theme.of(context).textTheme.bodyMedium),
+            const SizedBox(height: 16),
+            Expanded(
+              child: _buildChatRoomList(),
+            ),
           ],
         ),
       ),
@@ -102,5 +134,60 @@ class _HomeScreenState extends State<HomeScreen> {
         },
       ),
     );
+  }
+
+  Widget _buildChatRoomList() {
+    if (_isLoading) {
+      return Center(
+        child: CircularProgressIndicator(
+          color: Colors.green,
+        ),
+      );
+    } else if (_errorMessage != null) {
+      return Center(
+        child: Text(
+          _errorMessage!,
+          style: TextStyle(color: Colors.red),
+        ),
+      );
+    } else if (_chatRooms.isEmpty) {
+      return Center(
+        child: Text(
+          "No chat rooms available",
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: _chatRooms.length,
+        itemBuilder: (context, index) {
+          final room = _chatRooms[index];
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 3,
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: ListTile(
+              title: Text(room['name']),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ChatScreen(),
+                    settings: RouteSettings(
+                      arguments: {
+                        'roomId': room['id'],
+                        'roomName': room['name'],
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      );
+    }
   }
 }
